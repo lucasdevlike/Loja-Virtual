8e0dad8b530094c4048049ac7af17a38f9a227d7
lucasfregonezi <lucasfregonezi@gmail.com>
2024-03-29 07:26:25 -0300

    Interface Banco de dados

---
+++ App/Models/Database/ConnectDatabase/ConnectMysqliDatabase.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models\Database\ConnectDatabase;
+
+use App\Interfaces\InterfaceConnectDatabase;
+use mysqli;
+
+class ConnectMysqliDatabase implements InterfaceConnectDatabase
+{
+    private $mysqli;
+
+    public function __construct()
+    {
+        $this->mysqli = new mysqli("localhost", "root", "", "loja_phpoo");
+
+    }
+
+    public function connectDatabase()
+    {
+        return $this->mysqli;
+    }
+}
