c9d711967997113df3b54038b382afa4fa8f87d5
lucasfregonezi <lucasfregonezi@gmail.com>
2024-03-29 08:07:24 -0300

    Criando o Model

---
+++ App/Models/Database/TypeDatabase/TypeMysqliDatabase.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Models\Database\TypeDatabase;
+
+use App\Interfaces\InterfaceTypeDatabase;
+use App\Models\Database\ConnectDatabase\Connection;
+use App\Models\Database\ConnectDatabase\ConnectMysqliDatabase;
+
+class TypeMysqliDatabase implements InterfaceTypeDatabase
+{
+    private $mysqli;
+    private $objMysqli;
+
+    public function __construct()
+    {
+        $mysqli = new Connection(new ConnectMysqliDatabase());
+    }
+
+    public function prepare($sql)
+    {
+        $this->objMysqli = $this->mysqli->prepare($sql);
+    }
+
+
+    public function bindValue($key, $value)
+    {
+        $type = substr(gettype($value), 0, 1);
+        $this->objMysqli->bind_param($type, $value);
+    }
+
+
+    public function execute()
+    {
+        $this->objMysqli->execute();
+    }
+
+
+    public function rowCount()
+    {
+        $this->objMysqli->num_rows();
+    }
+
+
+    public function fetch()
+    {
+        $this->objMysqli->get_result()->fetch_object();
+    }
+
+
+    public function fetchAll()
+    {
+        $data = [];
+        $result = $this->objMysqli->get_result();
+        while($resultFetch = $result->fetch_assoc()) {
+            $data[] = $resultFetch;
+        }
+        return $data;
+    }
+
+
+}
---
+++ App/Models/Model.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Database\TypeDatabase\TypePdoDatabase;
+use App\Models\Database\TypeDatabase\TypeMysqliDatabase;
+use App\Models\Database\TypeDatabase\TypeDatabase;
+
+class Model
+{
+    private $typeDatabase;
+
+    public function __construct()
+    {
+        $database = new TypeDatabase(new TypePdoDatabase());
+        $this->typeDatabase = $database->getDatabase();
+    }
+
+    public function fetchAll()
+    {
+
+    }
+
+    public function find()
+    {
+
+    }
+
+
+}
